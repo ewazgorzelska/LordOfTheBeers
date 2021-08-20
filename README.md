252567b997a881244466403c1c335f379860eb0b
Ewa Zgorzelska <ewa.naroznik@hotmail.com>
2021-08-20 17:55:36 +0200

    cra config

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lord Of The Beers
 An on-line shop with beers using the Punk API in React.js
+
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import GlobalStyle from 'styles/GlobalStyle';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
