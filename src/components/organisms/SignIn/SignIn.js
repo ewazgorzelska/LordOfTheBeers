4ac8f7dca99d162d7f7b8d5afb1aff76ce784762
Ewa Zgorzelska <ewa.naroznik@hotmail.com>
2021-11-09 17:42:53 +0100

    adding ts

--- src/components/organisms/SignIn/SignIn.js
+++ src/components/organisms/SignIn/SignIn.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import styled from "styled-components";
 import FormField from "components/molecules/FormField/FormField";
 import { FormTitle } from "components/atoms/FormTitle/FormTitle";
 import Button from "components/atoms/Button/Button";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Wrapper = styled.div`
   display: flex;
@@ -21,29 +26,110 @@ const SignUpButton = styled.button`
 
 const SignIn = () => {
   const [signInForm, setSignInForm] = useState(true);
+  const handleButtonSignUp = () => setSignInForm(false);
 
-  const handleChange = () => {};
+  const [inputsContent, setInputsContent] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      email: "",
+      password: "",
+    }
+  );
+  const { email, password } = inputsContent;
 
-  const handleSignUp = () => setSignInForm(false);
+  const handleInputChange = (e) => {
+    setInputsContent({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const auth = getAuth();
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        
+        alert(
+          `Your email or password is incorrect, please check your data, ${errorCode} ${errorMessage}`
+        );
+      });
+  };
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        alert(
+          `Email is already in use, sign in or use other email, ${errorCode} ${errorMessage}`
+        );
+      });
+  };
 
   return (
     <form>
       <Wrapper>
         <FormTitle>{signInForm ? "Sign In" : "Sign Up"}</FormTitle>
-        <FormField label="E-mail:" name="" value="" onChange={handleChange} />
-        <FormField label="Password:" name="" value="" onChange={handleChange} />
+        <FormField
+          label="E-mail:"
+          htmlFor="email-input"
+          name="e-mail"
+          type="email"
+          id="email-input"
+          onChange={handleInputChange}
+          value={email}
+          required
+        />
+        <FormField
+          label="Password:"
+          htmlFor="password-input"
+          name="password"
+          type="password"
+          id="password-input"
+          onChange={handleInputChange}
+          value={password}
+          required
+        />
         {signInForm && (
-          <SignUpButton onClick={handleSignUp}>Sign Up!</SignUpButton>
+          <SignUpButton type="button" onClick={handleButtonSignUp}>
+            Sign Up!
+          </SignUpButton>
         )}
         {!signInForm && (
           <FormField
             label="Confirm password:"
-            name=""
-            value=""
-            onChange={handleChange}
+            htmlFor="confirm-password-input"
+            name="confirm-password"
+            type="password"
+            id="confirm-password-input"
+            onChange={handleInputChange}
+            value={password}
+            required
           />
         )}
-        <Button>Submit</Button>
+        <Button
+          type="submit"
+          name="submit"
+          value="1"
+          onClick={signInForm ? handleSignIn : handleSignUp}
+        >
+          Submit
+        </Button>
       </Wrapper>
     </form>
   );
